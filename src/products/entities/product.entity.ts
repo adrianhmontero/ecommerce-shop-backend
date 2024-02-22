9dc57f46544a871ff475dc59058050567b99a270
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-02-22 01:28:48 -0600

    Adding class validator packages , adding preffix and useGlobalPipes in main.ts. Also, setting create product dto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,7 +9,7 @@ export class Product {
    * El decorador Column nos permite crear columnas para nuestras entidades.
    * El primer parámetro es el tipo de dato que contendrá dicha columna.
    * Es importante revisar que el tipo de dato que se haya pasado sea compatible
-   * con TypeOrm.
+   * con postgres.
    * El segundo parámetro nos permite definir reglas para la creación de nuestros
    * registros.
    */
@@ -17,4 +17,30 @@ export class Product {
     unique: true,
   })
   title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    unique: true,
+    type: 'text',
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column({ type: 'text', array: true })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
 }
