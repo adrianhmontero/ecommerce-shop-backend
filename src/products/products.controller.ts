78784027f6c09e86c9dcfef3a264b642b6fd96b2
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-02-23 02:17:19 -0600

    Starting to set Product CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id/parse-mongo-id.pipe';
 
 @Controller('products')
 export class ProductsController {
@@ -17,18 +27,21 @@ export class ProductsController {
     return this.productsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  @Get(':term')
+  findOne(@Param('term', ParseUUIDPipe) term: string) {
+    return this.productsService.findOne(term);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  @Patch(':term')
+  update(
+    @Param('term') term: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(term, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.remove(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,12 +3,14 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
+import { isValidObjectId } from 'mongoose';
 
 @Injectable()
 export class ProductsService {
@@ -29,20 +31,46 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(term: string) {
+    let product: Product;
+
+    if (!product && isValidObjectId(term))
+      product = await this.productRepository.findOneBy({ id: term });
+    else product = await this.productRepository.findOneBy({ slug: term });
+
+    if (!product)
+      throw new NotFoundException(`Product with id or slug ${term} not found.`);
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(term: string, updateProductDto: UpdateProductDto) {
+    const productToUpdate = await this.findOne(term);
+
+    if (!productToUpdate)
+      throw new NotFoundException(`Product with id or slug ${term} not found.`);
+
+    try {
+      const { id } = productToUpdate;
+      const productUpdated = { ...productToUpdate, ...updateProductDto, id };
+
+      const product = this.productRepository.save(productUpdated);
+
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const productToDelete = await this.findOne(id);
+    await this.productRepository.remove(productToDelete);
+
+    return;
   }
 
   private handleDBExceptions(error: any) {
@@ -50,6 +78,26 @@ export class ProductsService {
     this.logger.error(error);
     throw new InternalServerErrorException(
       'Unexpected error. Check server logs.',
+    );
+  }
+
+  private handleExceptions(error: any, method: string = 'GET') {
+    const methods = {
+      PATCH: 'update',
+      GET: 'get',
+      POST: 'create',
+    };
+    /**
+     * Este código 11000 indica que ya existe un registro en DB que coincide con ese valor.  */
+    if (error.code === 11000)
+      throw new BadRequestException(
+        `Product exists in DB ${JSON.stringify(error.keyValue)}`,
+      );
+    /**
+     * Si no es error 11000, tenemos que revisar qué salió mal, y para indicar esto al frontend necesitamos usar el siguiente Exception Filter
+     */
+    throw new InternalServerErrorException(
+      `Can not ${methods[method]} Product - Check server logs`,
     );
   }
 }
