2d94375805d42ef12fc5190b20db9bf80793d082
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-02-28 00:05:53 -0600

    Adding tags property in Productr class

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -50,6 +50,13 @@ export class Product {
   @Column('text')
   gender: string;
 
+  @Column({
+    type: 'text',
+    array: true,
+    default: [],
+  })
+  tags: string[];
+
   @BeforeInsert()
   checkSlugInsert() {
     if (!this.slug) {
