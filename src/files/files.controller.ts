51a9a1a670dd9d941997af9d6c0acbb206081f49
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-03-13 01:33:39 -0600

    Starting to save images into diskStorage

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,15 +8,23 @@ import {
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter,
+      // limits: { fileSize: 1000 },
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-
     if (!file)
       throw new BadRequestException('Make sure that the file is an image.');
 
